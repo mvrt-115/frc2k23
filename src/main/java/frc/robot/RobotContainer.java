629364d0624dc1d8b32c68897f8f9b87be3728ed
Immortal747 <raggie052014@gmail.com>
2023-01-10 18:59:28 -0800

    elevator

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,8 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.SetElevatorHeight;
+import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -21,6 +23,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private Elevator elevator;
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -30,6 +33,8 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+    elevator = new Elevator();
+    elevator.setDefaultCommand(new SetElevatorHeight(elevator));
   }
 
   /**
---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,70 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Elevator extends SubsystemBase
+{
+  private ElevatorState currState;
+  private SupplyCurrentLimitConfiguration currentConfig;
+
+  public enum ElevatorState {
+    ZEROED, MID, HIGH, SHELF, FREE
+  };
+  
+  private TalonFX elev_motor;
+  
+  /** Creates a new Elevator. */
+  public Elevator()
+  {
+    elev_motor = new TalonFX(Constants.Elevator.MOTOR_ID);
+
+    elev_motor.configFactoryDefault();
+
+     // Sets up PIDF
+     elev_motor.config_kP(Constants.Elevator.kPIDIdx, Constants.Elevator.P);
+     elev_motor.config_kP(Constants.Elevator.kPIDIdx, Constants.Elevator.I);
+     elev_motor.config_kP(Constants.Elevator.kPIDIdx, Constants.Elevator.D);
+     elev_motor.config_kP(Constants.Elevator.kPIDIdx, Constants.Elevator.F);
+
+    elev_motor.setNeutralMode(NeutralMode.Brake);
+
+    elev_motor.setSelectedSensorPosition(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Elevator Height", elev_motor.getSelectedSensorPosition());
+  }
+
+  public void setHeightRaw(double targetHeightRaw)
+  {
+    elev_motor.set(ControlMode.Position, targetHeightRaw);
+  }
+
+  public void setHeight(double targetHeight)
+  {
+    // Checks bounds
+    targetHeight = targetHeight > Constants.Elevator.MAX_HEIGHT ? Constants.Elevator.MAX_HEIGHT:targetHeight;
+    targetHeight = targetHeight < Constants.Elevator.MIN_HEIGHT ? Constants.Elevator.MIN_HEIGHT:targetHeight;
+    
+    setHeightRaw(targetHeight/Constants.Elevator.INCHES_PER_TICK);
+  }
+
+  public double getHeight()
+  {
+    return elev_motor.getSelectedSensorPosition()*Constants.Elevator.INCHES_PER_TICK;
+  }
+}
