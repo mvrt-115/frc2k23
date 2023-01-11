a9f99f15dd0c6164412a0ff92749e4f6326bbf92
Immortal747 <raggie052014@gmail.com>
2023-01-10 18:59:28 -0800

    elevator

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,8 @@ import frc.robot.commands.AutonPathExample;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.SwerveJoystickCommand;
+import frc.robot.commands.SetElevatorHeight;
+import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.SwerveDrivetrain;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -29,6 +31,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final SwerveDrivetrain swerveDrivetrain = new SwerveDrivetrain();
+  private Elevator elevator;
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   // private final CommandXboxController m_driverController =
@@ -49,6 +52,8 @@ public class RobotContainer {
       driveJoystick));
     // Configure the trigger bindings
     configureBindings();
+    elevator = new Elevator();
+    elevator.setDefaultCommand(new SetElevatorHeight(elevator));
   }
 
   /**
