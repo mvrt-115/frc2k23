a135fb029f96eaa95f3ce9f253c170dd5efb8dca
Immortal747 <raggie052014@gmail.com>
2023-01-10 19:17:49 -0800

    kfhngsgihsha

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -34,5 +36,26 @@ public final class Constants {
     // Min/Max heights for the elevator (in inches)
     public static final double MAX_HEIGHT = 20;
     public static final double MIN_HEIGHT = 0;
+
+    public static final double ZERO_HEIGHT = 0;
+    public static final double SHELF_HEIGHT = 0;
+
+    // MID, HIGH heights parwa cone
+    public static final double MID_HEIGHT = 0;
+    public static final double HIGH_HEIGHT = 0;
+
+    // MID, HIGH heights para cube
+    public static final double MID_HEIGHT = 0;
+    public static final double HIGH_HEIGHT = 0;
+
+    // TrapezoidProfile State constants
+    public static final TrapezoidProfile.State ZERO_STATE = new TrapezoidProfile.State(Constants.Elevator.MID_HEIGHT, 0);
+    public static final TrapezoidProfile.State MID_STATE = new TrapezoidProfile.State(Constants.Elevator.MID_HEIGHT, 0);
+    public static final TrapezoidProfile.State HIGH_STATE = new TrapezoidProfile.State(Constants.Elevator.MID_HEIGHT, 0);
+    public static final TrapezoidProfile.State SHEL_STATE = new TrapezoidProfile.State(Constants.Elevator.MID_HEIGHT, 0);
+
+    // Game Object Heights
+    public static final double CONE_HEIGHT = 6;
+    public static final double CUBE_HIEGHT = 8;
   }
 }
