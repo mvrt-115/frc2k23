e87fc680e676277c1296ddbfd74842077ceb8a6d
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2023-01-11 18:23:34 -0800

    ?

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,61 +19,61 @@ import edu.wpi.first.math.util.Units;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
+    public static class OperatorConstants {
+        public static final int kDriverControllerPort = 0;
+    }
 
-  public static class VisionConstants{
-    public static final String kCameraName = "sheeshcam";
-    public static final Map<Integer, Pose3d> aprilTags = //relative to corner closest to tag 8
-    Map.of(
-        1,
-        new Pose3d(
-            Units.inchesToMeters(610.77),
-            Units.inchesToMeters(42.19),
-            Units.inchesToMeters(18.22),
-            new Rotation3d(0.0, 0.0, Math.PI)),
-        2,
-        new Pose3d(
-            Units.inchesToMeters(610.77),
-            Units.inchesToMeters(108.19),
-            Units.inchesToMeters(18.22),
-            new Rotation3d(0.0, 0.0, Math.PI)),
-        3,
-        new Pose3d(
-            Units.inchesToMeters(610.77),
-            Units.inchesToMeters(174.19), // FIRST's diagram has a typo (it says 147.19)
-            Units.inchesToMeters(18.22),
-            new Rotation3d(0.0, 0.0, Math.PI)),
-        4,
-        new Pose3d(
-            Units.inchesToMeters(636.96),
-            Units.inchesToMeters(265.74),
-            Units.inchesToMeters(27.38),
-            new Rotation3d(0.0, 0.0, Math.PI)),
-        5,
-        new Pose3d(
-            Units.inchesToMeters(14.25),
-            Units.inchesToMeters(265.74),
-            Units.inchesToMeters(27.38),
-            new Rotation3d()),
-        6,
-        new Pose3d(
-            Units.inchesToMeters(40.45),
-            Units.inchesToMeters(174.19), // FIRST's diagram has a typo (it says 147.19)
-            Units.inchesToMeters(18.22),
-            new Rotation3d()),
-        7,
-        new Pose3d(
-            Units.inchesToMeters(40.45),
-            Units.inchesToMeters(108.19),
-            Units.inchesToMeters(18.22),
-            new Rotation3d()),
-        8,
-        new Pose3d(
-            Units.inchesToMeters(40.45),
-            Units.inchesToMeters(42.19),
-            Units.inchesToMeters(18.22),
-            new Rotation3d()));
-  }
+    public static class VisionConstants{
+        public static final String kCameraName = "sheeshcam";
+        public static final Map<Integer, Pose3d> aprilTags = //relative to corner closest to tag 8
+        Map.of(
+            1,
+            new Pose3d(
+                Units.inchesToMeters(610.77),
+                Units.inchesToMeters(42.19),
+                Units.inchesToMeters(18.22),
+                new Rotation3d(0.0, 0.0, Math.PI)),
+            2,
+            new Pose3d(
+                Units.inchesToMeters(610.77),
+                Units.inchesToMeters(108.19),
+                Units.inchesToMeters(18.22),
+                new Rotation3d(0.0, 0.0, Math.PI)),
+            3,
+            new Pose3d(
+                Units.inchesToMeters(610.77),
+                Units.inchesToMeters(174.19), // FIRST's diagram has a typo (it says 147.19)
+                Units.inchesToMeters(18.22),
+                new Rotation3d(0.0, 0.0, Math.PI)),
+            4,
+            new Pose3d(
+                Units.inchesToMeters(636.96),
+                Units.inchesToMeters(265.74),
+                Units.inchesToMeters(27.38),
+                new Rotation3d(0.0, 0.0, Math.PI)),
+            5,
+            new Pose3d(
+                Units.inchesToMeters(14.25),
+                Units.inchesToMeters(265.74),
+                Units.inchesToMeters(27.38),
+                new Rotation3d()),
+            6,
+            new Pose3d(
+                Units.inchesToMeters(40.45),
+                Units.inchesToMeters(174.19), // FIRST's diagram has a typo (it says 147.19)
+                Units.inchesToMeters(18.22),
+                new Rotation3d()),
+            7,
+            new Pose3d(
+                Units.inchesToMeters(40.45),
+                Units.inchesToMeters(108.19),
+                Units.inchesToMeters(18.22),
+                new Rotation3d()),
+            8,
+            new Pose3d(
+                Units.inchesToMeters(40.45),
+                Units.inchesToMeters(42.19),
+                Units.inchesToMeters(18.22),
+                new Rotation3d()));
+        }
 }
