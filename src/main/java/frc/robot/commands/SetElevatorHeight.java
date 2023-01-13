933ff8163ff1e5fe8c5607c3d8b27b70250a9cca
Immortal747 <raggie052014@gmail.com>
2023-01-12 23:56:40 -0800

    quick fix

--- src/main/java/frc/robot/commands/SetElevatorHeight.java
+++ src/main/java/frc/robot/commands/SetElevatorHeight.java
@@ -42,6 +42,6 @@ public class SetElevatorHeight extends CommandBase
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (elevator.currentIsWithinError());
+    return true;
   }
 }
