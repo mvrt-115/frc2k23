b0ade11a03691b5084bae096aadc0bbdb603f72a
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2023-02-08 02:46:20 -0800

    delete this stuff

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc2k23";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 148;
-  public static final String GIT_SHA = "be44d834ee72ac394c42e434166bd0612a454209";
-  public static final String GIT_DATE = "2023-02-06 00:32:37 GMT";
+  public static final int GIT_REVISION = 149;
+  public static final String GIT_SHA = "fc73a60cf08b102dfa619b4ceccbeaa92315aae1";
+  public static final String GIT_DATE = "2023-02-06 01:15:39 GMT";
   public static final String GIT_BRANCH = "vision";
-  public static final String BUILD_DATE = "2023-02-06 01:13:05 GMT";
-  public static final long BUILD_UNIX_TIME = 1675645985201L;
+  public static final String BUILD_DATE = "2023-02-08 10:42:46 GMT";
+  public static final long BUILD_UNIX_TIME = 1675852966530L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
