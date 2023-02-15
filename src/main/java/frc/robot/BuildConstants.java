519b922b813371d173750ed335d44f47e705e0c7
QuackingBob <quackingbob101@gmail.com>
2023-02-14 21:34:10 -0800

    update paths

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc2k23";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 206;
-  public static final String GIT_SHA = "522726fd3819c9467bcf6f3fb0ceb368316c2bb3";
-  public static final String GIT_DATE = "2023-02-15 04:13:20 GMT";
+  public static final int GIT_REVISION = 209;
+  public static final String GIT_SHA = "712f6f3d8faf41a3704e9980cb22cb12d8046d38";
+  public static final String GIT_DATE = "2023-02-15 04:35:34 GMT";
   public static final String GIT_BRANCH = "swerve";
-  public static final String BUILD_DATE = "2023-02-15 04:28:50 GMT";
-  public static final long BUILD_UNIX_TIME = 1676435330843L;
+  public static final String BUILD_DATE = "2023-02-15 04:37:52 GMT";
+  public static final long BUILD_UNIX_TIME = 1676435872906L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/commands/AutonPathExample.java
+++ src/main/java/frc/robot/commands/AutonPathExample.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -18,6 +21,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.Constants;
 import frc.robot.subsystems.SwerveDrivetrain;
 
 public class AutonPathExample extends SequentialCommandGroup {
@@ -31,22 +35,19 @@ public class AutonPathExample extends SequentialCommandGroup {
     this.swerveDrivetrain = drivetrain;
     addRequirements(swerveDrivetrain);
 
-    trajectory = TrajectoryGenerator.generateTrajectory(
-      new Pose2d(0, 0, new Rotation2d(0)),
-      List.of(
-        new Translation2d(1, 0),
-        new Translation2d(1, 1),
-        new Translation2d(0, 1)
-      ), 
-      new Pose2d(0, 2, Rotation2d.fromDegrees(0.0)),
-      swerveDrivetrain.getTrajectoryConfig());
-    
-    try {
-      trajectory = TrajectoryUtil.fromPathweaverJson(Path.of("pathplanner/ScoreAndLevel.path"));
-    }
-    catch (IOException e) {
-      e.printStackTrace();
-    }
+    // trajectory = TrajectoryGenerator.generateTrajectory(
+    //   new Pose2d(0, 0, new Rotation2d(0)),
+    //   List.of(
+    //     new Translation2d(1, 0),
+    //     new Translation2d(1, 1),
+    //     new Translation2d(0, 1)
+    //   ), 
+    //   new Pose2d(0, 2, Rotation2d.fromDegrees(0.0)),
+    //   swerveDrivetrain.getTrajectoryConfig());
+
+    trajectory = PathPlanner.loadPath("ScoreAndLevel", new PathConstraints(
+      Constants.SwerveDrivetrain.kDriveMaxSpeedMPS/4.0, 
+      Constants.SwerveDrivetrain.kDriveMaxAcceleration/4.0));
     
     swerveDrivetrain.getField().getObject("traj").setTrajectory(trajectory);
 
