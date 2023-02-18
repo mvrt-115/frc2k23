2cd071cc3d0eaae846c5010017ca616fddcf1ec2
QuackingBob <quackingbob101@gmail.com>
2023-02-17 16:23:19 -0800

    constants build changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc2k23";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 214;
-  public static final String GIT_SHA = "8c70c050ad185daf127f9c4c2c78cd5b8963149a";
-  public static final String GIT_DATE = "2023-02-17 01:01:19 GMT";
-  public static final String GIT_BRANCH = "swerve";
-  public static final String BUILD_DATE = "2023-02-18 00:13:04 GMT";
-  public static final long BUILD_UNIX_TIME = 1676679184921L;
+  public static final int GIT_REVISION = 262;
+  public static final String GIT_SHA = "f6eaccbd8943a5bf00a8ad034e51a01e5bb66b77";
+  public static final String GIT_DATE = "2023-02-18 00:19:10 GMT";
+  public static final String GIT_BRANCH = "master";
+  public static final String BUILD_DATE = "2023-02-18 00:22:26 GMT";
+  public static final long BUILD_UNIX_TIME = 1676679746068L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
