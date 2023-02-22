9bb2518224873fdf163f83bedc54b7f710e983fa
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2023-02-22 01:24:47 -0800

    very nice music playing - runs music on stick

--- music/soundplayer/Music.java
+++ music/soundplayer/Music.java
@@ -6,24 +6,40 @@ import java.util.ArrayList;
 public class Music {
     
     public static void main(String[] args){
-        String s = null;
         ArrayList<String> files = new ArrayList<>();
 
         try {
-            Process p = Runtime.getRuntime().exec("ls /media/lilja/stick");
+            Process search = Runtime.getRuntime().exec("ls /media/lilja/stick");
             
             BufferedReader stdInput = new BufferedReader(new 
-                 InputStreamReader(p.getInputStream()));
+                 InputStreamReader(search.getInputStream()));
 
-            // read the output from the command
+            String s = null;
+
             while ((s = stdInput.readLine()) != null) {
                 files.add(s);
+            }
+
+            for (int x = 0; x < files.size(); x++){
+                Process play = Runtime.getRuntime().exec("mpg123 /media/lilja/stick/" + files.get(x));
+                
+                //This is an excelent way to do this
+                while (!processIsTerminated(play)){
+                    //Wait for process to finish...
+                }            
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        
+    private static boolean processIsTerminated (Process process) {
+        try {
+            process.exitValue();
+        } catch (IllegalThreadStateException itse) {
+            return false;
+        }
+        return true;
     }
 }
