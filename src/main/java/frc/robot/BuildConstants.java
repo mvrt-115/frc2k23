1ffde4ca87ddbaf1453869ce3ae848f67c8cfb5a
Shubham Jain <zoom2shubham@gmail.com>
2023-02-23 18:21:53 -0800

    auton changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc2k23";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 280;
-  public static final String GIT_SHA = "afbf8007b2baabf2da4318dedccb0b60567c18a1";
-  public static final String GIT_DATE = "2023-02-23 17:10:13 GMT";
+  public static final int GIT_REVISION = 283;
+  public static final String GIT_SHA = "d40bc83267931f8a263a971cf366297177435447";
+  public static final String GIT_DATE = "2023-02-24 00:31:12 GMT";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2023-02-23 17:28:55 GMT";
-  public static final long BUILD_UNIX_TIME = 1677173335142L;
+  public static final String BUILD_DATE = "2023-02-24 02:14:53 GMT";
+  public static final long BUILD_UNIX_TIME = 1677204893247L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
