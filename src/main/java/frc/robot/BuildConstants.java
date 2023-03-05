6bc265ada78613014fa07eea2ba2b1740722409a
QuackingBob <quackingbob101@gmail.com>
2023-03-04 22:35:35 -0800

    wpilib version fix

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc2k23";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 314;
-  public static final String GIT_SHA = "e8fc6df7b3176f1a676fd1650860e88de827f283";
-  public static final String GIT_DATE = "2023-03-05 00:22:58 GMT";
+  public static final int GIT_REVISION = 315;
+  public static final String GIT_SHA = "25b6fd54251723903c9ac393a166ad4d8bd315c6";
+  public static final String GIT_DATE = "2023-03-05 04:27:17 GMT";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2023-03-05 02:29:21 GMT";
-  public static final long BUILD_UNIX_TIME = 1677983361160L;
+  public static final String BUILD_DATE = "2023-03-05 06:32:53 GMT";
+  public static final long BUILD_UNIX_TIME = 1677997973651L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
