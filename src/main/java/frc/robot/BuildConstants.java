25b6fd54251723903c9ac393a166ad4d8bd315c6
QuackingBob <quackingbob101@gmail.com>
2023-03-04 20:27:17 -0800

    update to 2k23 and add drive shuffleboard layout

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc2k23";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 313;
-  public static final String GIT_SHA = "c653bfc632811a1344d8883e8ee90b594f64cd6b";
-  public static final String GIT_DATE = "2023-03-04 23:34:39 GMT";
+  public static final int GIT_REVISION = 314;
+  public static final String GIT_SHA = "e8fc6df7b3176f1a676fd1650860e88de827f283";
+  public static final String GIT_DATE = "2023-03-05 00:22:58 GMT";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2023-03-05 00:02:46 GMT";
-  public static final long BUILD_UNIX_TIME = 1677974566620L;
+  public static final String BUILD_DATE = "2023-03-05 02:29:21 GMT";
+  public static final long BUILD_UNIX_TIME = 1677983361160L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
