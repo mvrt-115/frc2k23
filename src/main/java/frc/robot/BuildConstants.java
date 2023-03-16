78b140d2e78a498a24710826b880330da008db37
Joshua <joshuahyau@gmail.com>
2023-03-16 16:28:51 -0700

    e

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc2k23";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 366;
-  public static final String GIT_SHA = "7f40bacd44b0a1ddda53c057d1cc514ca5b169aa";
-  public static final String GIT_DATE = "2023-03-16 03:33:45 GMT";
+  public static final int GIT_REVISION = 371;
+  public static final String GIT_SHA = "ca6949156007e9245be3cdaa18da1415614c7ca4";
+  public static final String GIT_DATE = "2023-03-16 23:19:11 GMT";
   public static final String GIT_BRANCH = "prematureComp";
-  public static final String BUILD_DATE = "2023-03-16 23:16:37 GMT";
-  public static final long BUILD_UNIX_TIME = 1679008597884L;
+  public static final String BUILD_DATE = "2023-03-16 23:27:44 GMT";
+  public static final long BUILD_UNIX_TIME = 1679009264913L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
