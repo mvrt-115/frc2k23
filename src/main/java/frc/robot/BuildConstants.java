180981b4a56b5c16a6e4dce77a9550685f50fd7d
QuackingBob <quackingbob101@gmail.com>
2023-03-24 08:11:55 -0700

    update

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc2k23";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 399;
-  public static final String GIT_SHA = "3a9250a7428eca281fbc5c8b71899c88a3fe52d9";
-  public static final String GIT_DATE = "2023-03-21 10:08:45 GMT";
+  public static final int GIT_REVISION = 400;
+  public static final String GIT_SHA = "dee7a1ad5bfbb9f7821879ebe84813b762dbdbe9";
+  public static final String GIT_DATE = "2023-03-24 02:26:21 GMT";
   public static final String GIT_BRANCH = "vision-odometry-testing";
-  public static final String BUILD_DATE = "2023-03-24 02:13:25 GMT";
-  public static final long BUILD_UNIX_TIME = 1679624005434L;
+  public static final String BUILD_DATE = "2023-03-24 02:37:01 GMT";
+  public static final long BUILD_UNIX_TIME = 1679625421147L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
