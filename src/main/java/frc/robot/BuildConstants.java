92f2c8f993e08286df5fa01634dab9786671b25d
QuackingBob <quackingbob101@gmail.com>
2023-03-23 18:23:46 -0700

    add 3d robo sim

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc2k23";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 383;
-  public static final String GIT_SHA = "83afa44991bd878550832dd5516031a2631c5c7e";
-  public static final String GIT_DATE = "2023-03-18 01:53:15 GMT";
+  public static final int GIT_REVISION = 387;
+  public static final String GIT_SHA = "615a8c70f2db09e70344590e5245b44123dd15cf";
+  public static final String GIT_DATE = "2023-03-18 04:40:52 GMT";
   public static final String GIT_BRANCH = "prematureComp";
-  public static final String BUILD_DATE = "2023-03-18 02:10:09 GMT";
-  public static final long BUILD_UNIX_TIME = 1679105409402L;
+  public static final String BUILD_DATE = "2023-03-23 21:23:28 GMT";
+  public static final long BUILD_UNIX_TIME = 1679606608389L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/DriveSimulationData.java
+++ src/main/java/frc/robot/subsystems/DriveSimulationData.java
@@ -4,16 +4,22 @@
 
 package frc.robot.subsystems;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Quaternion;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.simulation.AnalogGyroSim;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.utils.JoystickIO;
 
 /** Add your docs here. 
  * This class maintains simulation and field data as well as heading angle
@@ -28,6 +34,21 @@ public class DriveSimulationData {
     public static double currentTime;
     public static double prevTime;
 
+    private Logger logger;
+    private JoystickIO simJoystick = new JoystickIO(2);
+    private Pose3d groundIntakePose = new Pose3d(new Translation3d(0.43999999999999995, -0.04, 0.2999999999999998), new Rotation3d(0.0, -Math.PI/2.0, 0.0));
+    private Pose3d stage2Pose = new Pose3d(new Translation3d(-0.1538169326817099,0.3400000000000002,0.4434143993148704), new Rotation3d(new Quaternion(-0.5,-0.5,0.5,0.5).toRotationVector())); // new Rotation3d(0.0, Math.PI, Math.PI/2.0)
+    // private Pose3d stage3Pose = new Pose3d(new Translation3d(-0.024532990951728118, -0.25000000000000017, 0.35656511234797406), new Rotation3d(new Quaternion(-0.05594691700874833,0.7048900215474868,0.7048900215474866,-0.05594691700874836).toRotationVector()));
+    private Pose3d stage3Pose = new Pose3d(new Translation3d(-0.15453299095172823,0.25000000000000017,0.45656511234797414), new Rotation3d(new Quaternion(0.5000000000000003,0.49999999999999994,-0.49999999999999983,-0.5000000000000001)));
+    private Pose3d carriagePose = new Pose3d(new Translation3d(-0.3900000000000001,-0.7400000000000005,1.290000000000001), new Rotation3d(new Quaternion(0.5000000000000001,0.5,-0.4999999999999999,-0.5)));
+    // private Pose3d offsetPose = new Pose3d(new Translation3d(1, 0, 1), new Rotation3d());
+    private Translation3d offsetPose = new Translation3d(0.3, -0.04, 0.2);
+    
+    private double elev_x = 0;
+    private double elev_z = 0;
+    private double ground_rot = 0;
+    private double elev_angle = Math.toRadians(39.8);
+    
     /**
      * Create a new SimulationData container
      * @param odometry simulation odometry
@@ -39,6 +60,26 @@ public class DriveSimulationData {
         headingAngle = 0;
         currentTime = Timer.getFPGATimestamp();
         prevTime = currentTime;
+        logger = Logger.getInstance();
+
+        // used when finding zero positions of components
+        // simJoystick.button(1).onTrue(new InstantCommand(() -> stage3Pose = stage3Pose.transformBy(new Transform3d(new Translation3d(0.01, 0, 0), new Rotation3d()))));
+        // simJoystick.button(3).onTrue(new InstantCommand(() -> stage3Pose = stage3Pose.transformBy(new Transform3d(new Translation3d(-0.01, 0, 0), new Rotation3d()))));
+        // simJoystick.button(2).onTrue(new InstantCommand(() -> stage3Pose = stage3Pose.transformBy(new Transform3d(new Translation3d(0, 0.01, 0), new Rotation3d()))));
+        // simJoystick.button(4).onTrue(new InstantCommand(() -> stage3Pose = stage3Pose.transformBy(new Transform3d(new Translation3d(0, -0.01, 0), new Rotation3d()))));
+        // simJoystick.button(5).onTrue(new InstantCommand(() -> stage3Pose = stage3Pose.transformBy(new Transform3d(new Translation3d(0, 0, 0.01), new Rotation3d()))));
+        // simJoystick.button(6).onTrue(new InstantCommand(() -> stage3Pose = stage3Pose.transformBy(new Transform3d(new Translation3d(0, 0, -0.01), new Rotation3d()))));
+        // simJoystick.button(7).onTrue(new InstantCommand(() -> stage3Pose = stage3Pose.transformBy(new Transform3d(new Translation3d(), new Rotation3d(Math.PI/10, 0.0, 0.0)))));
+        // simJoystick.button(8).onTrue(new InstantCommand(() -> stage3Pose = stage3Pose.transformBy(new Transform3d(new Translation3d(), new Rotation3d(-Math.PI/10, 0.0, 0.0)))));
+        // simJoystick.button(9).onTrue(new InstantCommand(() -> stage3Pose = stage3Pose.transformBy(new Transform3d(new Translation3d(), new Rotation3d(0.0, Math.PI/10, 0.0)))));
+        // simJoystick.button(10).onTrue(new InstantCommand(() -> stage3Pose = stage3Pose.transformBy(new Transform3d(new Translation3d(), new Rotation3d(0.0, -Math.PI/10, 0.0)))));
+        // simJoystick.button(11).onTrue(new InstantCommand(() -> stage3Pose = stage3Pose.transformBy(new Transform3d(new Translation3d(), new Rotation3d(0.0, 0.0, Math.PI/10)))));
+        // simJoystick.button(12).onTrue(new InstantCommand(() -> stage3Pose = stage3Pose.transformBy(new Transform3d(new Translation3d(), new Rotation3d(0.0, 0.0, -Math.PI/10)))));
+
+        // simJoystick.button(1).onTrue(new InstantCommand(() -> offsetPose = offsetPose.plus(new Translation3d(0.01, 0, 0))));
+        // simJoystick.button(3).onTrue(new InstantCommand(() -> offsetPose = offsetPose.plus(new Translation3d(-0.01, 0, 0))));
+        // simJoystick.button(2).onTrue(new InstantCommand(() -> offsetPose = offsetPose.plus(new Translation3d(0, 0, 0.01))));
+        // simJoystick.button(4).onTrue(new InstantCommand(() -> offsetPose = offsetPose.plus(new Translation3d(0, 0, -0.01))));
     }
 
     /**
@@ -56,6 +97,25 @@ public class DriveSimulationData {
             m_odometry.getPoseMeters().getX(), 
             m_odometry.getPoseMeters().getY(),
             m_odometry.getPoseMeters().getRotation());
+        
+        elev_x = Math.max(0, simJoystick.getX()) * 0.55 * Math.cos(elev_angle);
+        elev_z = Math.max(0, simJoystick.getX()) * 0.55 * Math.sin(elev_angle);
+        ground_rot = 0.83 * Math.PI * (Math.min(simJoystick.getRawAxis(1), 0));
+
+        Rotation3d R = new Rotation3d(0.0, ground_rot, 0.0);
+        Pose3d newGroundPose = rotateAroundPoint(groundIntakePose, R, offsetPose);
+
+        // T(x)=R(x−v)+v=Rx+(v−Rv)
+        // Transform3d rT = new Transform3d(new Translation3d(), R);
+        // Pose3d newGroundPose = groundIntakePose.transformBy(rT).plus(offsetPose.minus(offsetPose.transformBy(rT)));
+        // Pose3d newGroundPose = groundIntakePose.transformBy(new Transform3d(new Translation3d(), new Rotation3d(0.0, ground_rot, 0.0)));
+
+
+        logger.recordOutput("GroundIntakeComponent", newGroundPose); // .transformBy(new Transform3d(new Translation3d(), new Rotation3d(0.0, ground_rot, 0.0)))
+        logger.recordOutput("ground axis", groundIntakePose);
+        logger.recordOutput("Stage2Component", stage2Pose.transformBy(new Transform3d(new Translation3d(0.0, elev_z, -elev_x), new Rotation3d())));
+        logger.recordOutput("Stage3Component", stage3Pose.transformBy(new Transform3d(new Translation3d(0.0, 2*elev_z, -2*elev_x), new Rotation3d())));
+        logger.recordOutput("IntakeComponent", carriagePose.transformBy(new Transform3d(new Translation3d(0.0, 3*elev_z, -3*elev_x), new Rotation3d())));
     }
 
     /**
@@ -105,5 +165,16 @@ public class DriveSimulationData {
     public void resetOdometry(Rotation2d rotation2d, SwerveModulePosition[] modulePositions, Pose2d pose) {
         m_odometry.resetPosition(rotation2d, modulePositions, pose);
         // headingAngle = pose.getRotation().getRadians();
+    }
+
+    /**
+     * Rotate a point around a specified point in space by a rotation3d
+     * @param p original point
+     * @param R specified rotation
+     * @param o point around which to rotate
+     * @return transformed point
+     */
+    public static Pose3d rotateAroundPoint(Pose3d p, Rotation3d R, Translation3d o) {
+        return new Pose3d(p.getTranslation().minus(o).rotateBy(R).plus(o), p.getRotation().rotateBy(R));
     }
 }
